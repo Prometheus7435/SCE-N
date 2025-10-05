f982e6f87e221074670e4e9febde73652f704bc1
Zach Bombay <zacharybombay@gmail.com>
2025-10-05 17:46:27 -0400

    updated variables for new repo

--- scripts/install.sh
+++ scripts/install.sh
@@ -8,47 +8,15 @@ set -euo pipefail
 TARGET_HOST="${1:-}"
 TARGET_USER="${2:-shyfox}"
 
-# TARGET_HOST=""
-# TARGET_USER=""
+GIT_REPO="Prometheus7435/SCE-N.git"
 
-# if [ -n "${1}" ]; then
-#   TARGET_HOST="${1}"
-# else
-#   echo "ERROR! $(basename "${0}") requires a host name:"
-#   ls -1 host/ | grep -v ".nix" | grep -v _
-#   exit 1
-# fi
-
-# case "${TARGET_HOST}" in
-#   starbase|akira|fermi|odyssey) true;;
-#   *)
-#     echo "ERROR! ${TARGET_HOST} is not a supported host"
-#     exit 1
-#     ;;
-# esac
-
-# if [ -n "${2}" ]; then
-#   TARGET_USER="${2}"
-# else
-#   echo "ERROR! $(basename "${0}") requires a user name"
-#   ls -1 nixos/_mixins/users | grep -v root
-#   exit 1
-# fi
-
-# case "${TARGET_USER}" in
-#   starfleet|shyfox) true;;
-#   *)
-#     echo "ERROR! ${TARGET_USER} is not a supported user"
-#     exit 1
-#     ;;
-# esac
 if [ ! -d "$HOME/Zero/nix-config/.git" ]; then
-  git clone https://github.com/Prometheus7435/SCE-N.git "$HOME/Zero/nix-config"
+  git clone "https://github.com/$GIT_REPO" "$HOME/Zero/nix-config"
 fi
 
 pushd "$HOME/Zero/nix-config"
 
-if [ ! -e "nixos/${TARGET_HOST}/disks.nix" ]; then
+if [ ! -e "hosts/${TARGET_HOST}/disks.nix" ]; then
   echo "ERROR! $(basename "${0}") could not find the required nixos/${TARGET_HOST}/disks.nix"
   exit 1
 fi
@@ -72,25 +40,24 @@ if [[ $REPLY =~ ^[Yy]$ ]]; then
 	 --no-write-lock-file \
 	 -- \
 	 --mode zap_create_mount \
-	 "nixos/${TARGET_HOST}/disks.nix"
+	 "hosts/${TARGET_HOST}/disks.nix"
 
     sudo nixos-install --no-root-password --flake ".#${TARGET_HOST}"
 
     # Rsync my nix-config to the target install
     # mkdir -p ~/.local/state/nix/profiles
-    # mkdir -p "/mnt/home/${TARGET_USER}/Zero/nix-config"
+    mkdir -p "/mnt/home/${TARGET_USER}/Zero/nix-config"
     # rsync -a --delete "${PWD}/" "/mnt/home/${TARGET_USER}/Zero/nix-config/"
 
     # Rsync nix-config to the target install and set the remote origin to SSH.
     rsync -a --delete "$HOME/Zero/" "/mnt/home/$TARGET_USER/Zero/"
     pushd "/mnt/home/$TARGET_USER/Zero/nix-config"
-    git remote set-url origin git@github.com:Prometheus7435/nix-config.git
+    # git remote set-url origin git@github.com:Prometheus7435/SCE-N.git
+    git remote set-url origin "git@github.com:$GIT_REPO"
     popd
 
     mkdir -p "/mnt/home/${TARGET_USER}/.local/state/nix/profiles"
 
     ## folders used in my emacs config
-    # mkdir -p "/mnt/home/${TARGET_USER}/.config/emacs/backups"
-    # mkdir -p "/mnt/home/${TARGET_USER}/.config/emacs/emacs_autosave"
     mkdir -p "/mnt/home/${TARGET_USER}/.config/emacs/{backups,emacs_autosave}"
 fi
