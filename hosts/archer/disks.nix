e34579652a8b28faa4c6b6399e5cfcc142d00ae6
Zach Bombay <zacharybombay@gmail.com>
2025-10-06 22:27:17 -0400

    comment back keyphrase

--- hosts/archer/disks.nix
+++ hosts/archer/disks.nix
@@ -38,8 +38,8 @@ in
                 name = "crypted";
                 # disable settings.keyFile if you want to use interactive password entry
                 passwordFile = "/tmp/secret.key"; # Interactive
-                # keylocation = "prompt";
-                # keyformat = "passphrase";
+                keylocation = "prompt";
+                keyformat = "passphrase";
                 settings = {
                   allowDiscards = true;
                   #keyFile = "/tmp/secret.key";
