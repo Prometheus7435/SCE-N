05defd2c3a39a08d8fa67b66824e84228235b65a
Zach Bombay <zacharybombay@gmail.com>
2025-10-13 20:06:22 -0400

    remove modure import

--- hosts/archer/default.nix
+++ hosts/archer/default.nix
@@ -4,7 +4,7 @@
 # RAM: 16GB
 # NVME:
 
-{ inputs, lib, pkgs, username, modulesPath, desktop, ... }:
+{ inputs, lib, pkgs, username, desktop, ... }:
 let
   # pkgs = import <nixpkgs> {
   #   overlays = [
