70356637200f5206a9a0915743fc33f5db0a216f
Zach Bombay <zacharybombay@gmail.com>
2025-11-10 22:59:21 -0500

    comment out redundant allowUnfree bit

--- home/home.nix
+++ home/home.nix
@@ -22,7 +22,7 @@
     config = {
       allowUnfree = true;
       # Workaround for https://github.com/nix-community/home-manager/issues/2942
-      allowUnfreePredicate = (_: true);
+      # allowUnfreePredicate = (_: true);
     };
   };
 
